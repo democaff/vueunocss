30d3d919023dd972fee572dc81bd36990efbad8f
cyberobject <117184986+cyberobjec@users.noreply.github.com>
2024-04-17 22:26:54 +0800

    unocss- v1

--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "unocss";
+import presetAttributify from "@unocss/preset-attributify";
 import presetIcons from "@unocss/preset-icons";
 import presetUno from "@unocss/preset-uno";
 
 export default defineConfig({
   presets: [
+    presetAttributify({
+      /* preset options */
+    }),
     presetUno(),
     presetIcons({
       extraProperties: {
