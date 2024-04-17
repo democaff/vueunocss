025e8ccb9709afc8ee9782906ff7b38fc98fc178
cyberobject <117184986+cyberobjec@users.noreply.github.com>
2024-04-17 23:02:13 +0800

    unocss-v1a

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,30 @@
   <div
     class="i-twemoji-grinning-face-with-smiling-eyes hover:i-twemoji-face-with-tears-of-joy"
   />
+
+  <div m-2 rounded text-teal-400>hello</div>
+  <button
+    class="bg-blue-400 hover:bg-blue-500 text-sm text-white font-mono font-light py-2 px-4 rounded border-2 border-blue-200 dark:bg-blue-500 dark:hover:bg-blue-600"
+  >
+    Button
+  </button>
+  <button class="border border-red">Button</button>
+  <button border="~ red">Button</button>
+  <button border="~ red-500">Button</button>
+
+  <div border="~ red-500" w-100>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda iste
+    necessitatibus quisquam molestias distinctio ipsa similique nam. Facilis
+    cupiditate aut maiores accusantium nam, odio tenetur itaque aperiam
+    excepturi animi unde.
+  </div>
+
+  <p class="mt-10 bg-slate-500 p-10 text-lg text-white" lg="text-blue">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nesciunt
+    eveniet alias, facilis distinctio cumque quo quidem praesentium blanditiis
+    provident aliquid tempore laudantium mollitia incidunt dolorum quasi
+    adipisci, numquam assumenda?
+  </p>
 </template>
 
 <style scoped></style>
